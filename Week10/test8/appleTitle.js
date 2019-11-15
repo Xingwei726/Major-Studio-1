37f4c4ed495d309e98629e749499a5368c2e2f11
EC2 Default User <ec2-user@ip-172-31-15-86.ec2.internal>
2019-11-15 13:06:54 +0000

    updates

--- Week10/test8/appleTitle.js
+++ Week10/test8/appleTitle.js
@@ -58,10 +58,44 @@ var myColor = d3.scaleOrdinal()
   .domain(["no","yes"])
   .range("#194F39","#FFFAF0");
 
+var tooltip = d3.select("#dataviz")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("background-color", "#194F39")
+    .style("position", "absolute")
+    .style("display","inline")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "6px")
+    .style("opacity", 1)
 
+
+var mousemove3 = function(data) {
+    tooltip
+      .html("This apple was born in " + data.date + ".")
+      .style("color", "white")
+          // .attr("xlink:href", function(d) { return d.img;})
+      .style("left", (d3.mouse(this)[0]+40) + "px")
+      .style("top", (d3.mouse(this)[1]+800) + "px")//+150 reduce the distance between tooltip and mouse
+    // d3.select(this)
+    // .transition()
+    // .ease(d3.easeElastic)
+    //      .attr("width", 150)
+    //      .attr("height", 150)
+}
+
+// var mouseleave3 = function(data) {
+//     d3.select(this)
+//     .transition()
+//     .ease(d3.easeElastic)
+
+//          .attr("width", 80)
+//          .attr("height", 80)
+//          .style("fill-opacity", 1)
+// }  
 
               
-      var appleTitle = svg3.selectAll("circle")
+ var appleTitle = svg3.selectAll("circle")
                      .data(data)
                      .enter()
                      .append("circle")
@@ -71,6 +105,13 @@ var myColor = d3.scaleOrdinal()
                      .style("fill","none")
                      .attr("transform", "translate(55, 110)")
                      .style("opacity",0)
+                    .on("mousemove", mousemove3)
+                    .on('click', function(d, i) {
+                        window.open(d.metURL);
+                        const el = d3.select(this);
+                        el.transition()
+                        .duration(750)
+                    });
 
 //     }
 function f3() {
@@ -140,6 +181,7 @@ function f3() {
                         return 700
                         }
                     })
+                    
                     
 } 
 
